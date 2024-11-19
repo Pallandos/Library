64b4026d41d15462bf92dfab9c2038af060c95e1
Pallandos <146179143+Pallandos@users.noreply.github.com>
2024-11-19 13:15:37 +0100

    Update open_read.c

--- OS/TPTD1/open_read.c
+++ OS/TPTD1/open_read.c
@@ -32,7 +32,7 @@ main () {
             char bufferp[4];
             read(fd, bufferp, 4);
             printf("buffer père: %s\n", bufferp);
-            write(fd, "père", 4);
+            write(fd, "pere", 4);
 
             break;
     }
